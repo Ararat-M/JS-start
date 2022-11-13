1ffd4961ff5af4a71427b54e0a3626b7b6465ebe
Ararat-M <melkumyan2077@gmail.com>
2022-11-13 13:21:22 +0300

    update task2 and minor fix

---
+++ task2/script.js
@@ -0,0 +1,14 @@
+let consoleLog = document.querySelector("#consoleLog");
+consoleLog.addEventListener("click", () => {
+    alert("Метод для вывода сообщения в веб консоль");
+});
+
+let alertExample = document.querySelector("#alert")
+alertExample.addEventListener("click", () => {
+    alert("Функция для вывода информации пользователям")
+})
+
+let promptExample = document.querySelector("#prompt")
+promptExample.addEventListener("click", () => {
+    alert("Функция для ввода информации пользователем")
+})
